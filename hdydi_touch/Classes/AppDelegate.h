f80cd2323ac6ac24d9f7bd2c507c3231a58c135b
Emmett Butler <emmett.butler321@gmail.com>
2014-05-20 19:32:13 -0400

    init

---
+++ hdydi_touch/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  hdydi_touch
+//
+//  Created by Emmett Butler on 5/20/14.
+//  Copyright Star Maid 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
